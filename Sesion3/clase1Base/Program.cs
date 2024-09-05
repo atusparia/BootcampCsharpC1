008b0bdf4a622702903ac7ad982b9cc5a9157e7c
Ronald Zenón Cuevas Machaca <rzcuevasm@gmail.com>
2024-09-04 20:29:59 -0500

    actualizacion

--- Sesion3/clase1Base/Program.cs
+++ Sesion3/clase1Base/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola! Alumnos");
+//variables
+string nombre = "ronald";
+int edad = 43;
+
+Console.WriteLine("Escribe tu nombre");
+nombre = Console.ReadLine();
+Console.WriteLine("Mi nombre es " + nombre + " mi edad es " + edad);
